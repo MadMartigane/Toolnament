03fe5a386187a7523c8448d5f476d157ffd896b0
Michaël Baudchon <michael.baudchon@ovhcloud.com>
2023-04-20 09:09:06 +0200

    feat(component): add team name lite

---
+++ src/components/TeamNameTile.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export default {
+        name: "TeamNameTile",
+        props: {
+            name: String,
+            id: String
+        }
+    }
+</script>
+
+<template>
+    <div class="tile tile-primary">
+        <p>{{ name }}</p>
+    </div>
+</template>
+
--- src/views/GeneratorView.vue
+++ src/views/GeneratorView.vue
@@ -1,3 +1,49 @@
+
+<script lang="ts">
+    import TeamNameInput from "@/components/TeamNameInput.vue";
+import TeamNameTile from "@/components/TeamNameTile.vue";
+
+    export interface TeamModel {
+        name: string,
+        id: number
+    };
+
+    export default {
+        name: "GeneratorView",
+        data () {
+            return {
+                displayDiagram: false,
+                teamNumber: 16,
+                teamList: [] as Array<TeamModel>
+            }
+        },
+        methods: {
+            nubmerOfTeamChanges(): void {
+                console.log("[GeneratorView] teamNumber: ", this.teamNumber);
+                const teamListBackup = this.teamList.map((item: TeamModel) => item);
+
+                this.teamList.length = 0;
+                for (let i=0; i < this.teamNumber; i++) {
+                    this.teamList[i] = teamListBackup[i] || { name: `Team #${i}`, id: i };
+                }
+            },
+
+            generateDiagram (): void {
+                console.log("generator()");
+                this.displayDiagram = true;
+            }
+        },
+        components: {
+            TeamNameInput,
+            TeamNameTile
+        },
+        mounted() {
+            this.nubmerOfTeamChanges();
+        }
+    };
+</script>
+
+
 <template>
     <div class="h-screen">
         <div class="main-container">
@@ -5,13 +51,14 @@
                 <form action="" method="" class="form-main">
 
                     <div class="form-element">
-                        <label for="teamNumber">Nombre d'équipe: </label>
+                        <label for="teamNumber">Nombre d'équipe:</label>
                         <input
+                                v-model="teamNumber"
+                                @input=nubmerOfTeamChanges()
                                 class="input-element"
                                 min="0"
                                 max="32"
                                 type="number"
-                                value="32"
                                 step="2"
                                 name="teamNumber"
                                 id="form-team-number"
@@ -19,17 +66,39 @@
                     </div>
 
                     <div class="flex justify-center">
-                        <button class="btn-primary">Génération !</button>
-                        <button class="btn-secondary">Génération 2 !</button>
+                        <button
+                                @:click="generateDiagram()"
+                                type="button"
+                                class="btn-primary">Génération !</button>
+                        <button
+                                type="button"
+                                class="btn-secondary">Génération 2 !</button>
                     </div>
 
                 </form>
             </div>
 
             <div class="block-container">
-                <div id="form-team-container"></div>
+                <TeamNameInput
+                        v-for="team in teamList"
+                        :name="team.name"
+                        :id="team.id"
+                        />
             </div>
         </div>
+
+        <!-- diagram container -->
+        <div
+                v-if="displayDiagram"
+                class="tile-container">
+                    <TeamNameTile
+                        v-for="team in teamList"
+                        :name="team.name"
+                        />
+
+
+        </div>
+
     </div>
 </template>
 
